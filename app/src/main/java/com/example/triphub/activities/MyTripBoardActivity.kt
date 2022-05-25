406508ad7a125998d669f3e2aa3a948fd1700a18
Patryk Bandyra <patryk.bandyra@gmail.com>
2022-05-25 23:04:38 +0200

    trip board taskbar navigation

--- app/src/main/java/com/example/triphub/activities/MyTripBoardActivity.kt
+++ app/src/main/java/com/example/triphub/activities/MyTripBoardActivity.kt
@@ -1,5 +1,6 @@
 package com.example.triphub.activities
 
+import android.content.Intent
 import android.os.Bundle
 import com.example.triphub.databinding.ActivityMyTripBoardBinding
 
@@ -7,6 +8,22 @@ class MyTripBoardActivity : BaseActivity<ActivityMyTripBoardBinding>() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        setUpTaskBarNavigation()
+    }
+
+    private fun setUpTaskBarNavigation() {
+        binding.ivPeople.setOnClickListener {
+            startActivity(Intent(this, MyTripPeopleActivity::class.java))
+            finish()
+        }
+        binding.ivMap.setOnClickListener {
+            startActivity(Intent(this, MyTripMapActivity::class.java))
+            finish()
+        }
+        binding.ivChat.setOnClickListener {
+            startActivity(Intent(this, MyTripChatActivity::class.java))
+            finish()
+        }
     }
 
     override fun getViewBinding() = ActivityMyTripBoardBinding.inflate(layoutInflater)
